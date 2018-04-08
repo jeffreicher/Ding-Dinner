6b2b5108b36b847dbabe57d56a923b6a9e6fbab5
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-07 20:32:27 -0700

    Added icons and styling to myMeals page

--- client/src/components/meals.js
+++ client/src/components/meals.js
@@ -99,9 +99,7 @@ class Meals extends Component{
                 <footer className="mealsFooter">
                     <div className="mealsMyMeals"><Link to='/mymeals'><i className="far fa-list-alt"></i></Link></div>
                     <div className="mealsGroceries"><Link to='/grocery'><i className="fas fa-shopping-cart"></i></Link></div>
-                    <div className="mealsSettings"><Link to='/settings'><i className="fas fa-cogs"></i>
-
-</Link></div>
+                    <div className="mealsSettings"><Link to='/settings'><i className="fas fa-cogs"></i></Link></div>
                 </footer>
             </div>
         )

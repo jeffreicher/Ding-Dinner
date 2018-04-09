1b5b25ebb49cd28ec5fda5529dd49c863eaf3109
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-09 15:21:40 -0700

    Added header to pages missing a header and styling page needs to have background color added after removing margin from buttons

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import LogoHeader from './logo-header';
 
 class Settings extends Component{
     constructor(props){
@@ -8,6 +9,8 @@ class Settings extends Component{
     }
     render(){
         return (
+            <div>
+            <LogoHeader />
             <div className='settingsContainer'>
                 <h2 className='settingsHeader'>Settings</h2>
                 <button className='settingsButton'><Link to='/diet-settings'>Reset Filters</Link></button>
@@ -15,7 +18,9 @@ class Settings extends Component{
                 <button className='settingsButton'><Link to='meal-num-settings'>Add Meals</Link></button>
                 <br/>
                 <button className='settingsButton'><Link to='/'>Logout</Link></button>
+            </div>                
             </div>
+
         )
     }
 }

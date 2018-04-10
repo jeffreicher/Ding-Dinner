a12fbeba4afde63ff6633e9b8f5650f3f449bd1b
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-09 17:52:14 -0700

    Added header to details page

--- client/src/components/logo-header.js
+++ client/src/components/logo-header.js
@@ -6,7 +6,7 @@ class LogoHeader extends Component {
         console.log(this.props);
         return (
             <header className="groceryHeader z-depth-2">
-                    {(this.props.back) ? <div className="groceryBack"><Link to='/mymeals'><i style={{fontSize: `2rem`}} className="fas fa-arrow-left iconcolor"></i></Link></div> : ''}
+                    {(this.props.back) ? <div className="groceryBack" onClick={this.props.onClick}><Link to='/mymeals'><i style={{fontSize: `2rem`}} className="fas fa-arrow-left iconcolor"></i></Link></div> : ''}
                     <div className="groceryLogo lobsterFont">Ding</div>
                     {(this.props.add) ? <div className="groceryAdd"><Link to='/meal-number'><i style={{fontSize: `2rem`}} className="far fa-plus-square"></i></Link></div> : ''}
             </header>

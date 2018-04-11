96c86391f646e0c09522ea6541e060c54bbd4840
Nolan <nemerson17@gmail.com>
2018-04-10 21:44:52 -0700

    Adding endpoint headers again

--- endpoints/user_login.php
+++ endpoints/user_login.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'mysql_connect.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
+    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
 $_POST['email'] = "mkane3@something.com";
 $_POST['password'] = "password";
 

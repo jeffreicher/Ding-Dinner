7bc598db875f08be1c86a3a5da0623280344648c
Matthew <matthewjkane@live.com>
2018-04-12 11:27:00 -0700

    Added email_check.php endpoint for testing to see if the email is already taken

---
+++ endpoints/email_check.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+require_once 'mysqli_connect.php';
+
+/*header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
+    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");*/
+
+//Make PHP understand the Axios call
+//$entityBody = file_get_contents('php://input');
+//$request_data = json_decode($entityBody, true);
+
+$request_data['email'] = "mkane36@something.com";
+
+//Validate Email
+if (!($email = filter_var($request_data['email'], FILTER_VALIDATE_EMAIL))){
+    die('Your email is invalid');
+}
+
+//Prepared statement to SELECT user with matching email and password
+if(!($stmt = $myconn->prepare("SELECT `email` FROM `users` WHERE `email` = ? LIMIT 1"))){
+    die("Prepare failed: (" . $myconn->errno . ") " . $myconn->error);
+}
+
+//Binding parameters for the SELECT statement
+if(!$stmt->bind_param('s', $email)){
+    die("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+}
+
+//Execute the prepared SELECT query
+if(!$stmt->execute()){
+    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+}
+$stmt->store_result();
+if($stmt->num_rows > 0){
+    echo "email taken";
+} else {
+    echo "email available";
+}

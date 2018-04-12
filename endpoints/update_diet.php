4a5b6226945744eee00cb59d1e09976ad974c397
Matthew <matthewjkane@live.com>
2018-04-12 14:59:59 -0700

    fixed issues with headers on multiple lines

--- endpoints/update_diet.php
+++ endpoints/update_diet.php
@@ -5,8 +5,7 @@ require_once 'helper_functions.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
-    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 
 //Make PHP understand the Axios call
 $entityBody = file_get_contents('php://input');

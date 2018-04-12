43720eb1980b2c726ed2ffbf6004343ce22ec3af
Nolan <nemerson17@gmail.com>
2018-04-12 16:54:12 -0700

    Confirm only routes on successful server response

--- client/src/components/general/next_button.js
+++ client/src/components/general/next_button.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default (props) => (
     <div className="container">
-        <button className={`waves-effect waves-light btn-large green darken-2 oxygenFont z-depth-2 ${props.style}`}>{props.name || 'Next'}</button>
+        <button onClick={props.onclick} className={`waves-effect waves-light btn-large green darken-2 oxygenFont z-depth-2 ${props.style}`}>{props.name || 'Next'}</button>
     </div>
 );

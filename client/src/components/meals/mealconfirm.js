9614d86170e180b8bfa489eaea70f669e0e854f2
Nolan <nemerson17@gmail.com>
2018-04-12 15:56:56 -0700

    Minor updates to confirm button

--- client/src/components/meals/mealconfirm.js
+++ client/src/components/meals/mealconfirm.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 class MealConfirm extends Component{
 
+
+
     render(){
         return (
             <div className='mealConfirmContainer row'>
@@ -10,7 +12,7 @@ class MealConfirm extends Component{
                     <div>Press to finalize your selections!</div>
                 </div>
                 <div className='col s3 valign-wrapper'>
-                    <div className='btn green confirmButton left-align valign-wrapper'>
+                    <div className='btn green confirmButton left-align valign-wrapper' onClick={this.props.closeconfirm}>
                         <i className='material-icons confirmCheck' style={{fontSize: '3rem'}}>check</i>
                     </div>
                 </div>

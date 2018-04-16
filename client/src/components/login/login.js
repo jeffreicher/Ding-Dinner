361a2c8609234dd3935b7c0e353403a6c50d3464
Nolan <nemerson17@gmail.com>
2018-04-16 09:29:38 -0700

    Styling complete for static landing

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -49,7 +49,7 @@ class Login extends Component{
         return (
             <div className="loginContainer">
                 <img className='loginLogo' src={dingLogo} />
-                <h4 className='loginSlogan'>Dinner is served</h4>
+                <h4 className='loginSlogan'>dinner is served</h4>
                 <div className='loginButtons'>
                     {this.state.loginHide && <LoginHide history={this.props.history} returnFX={this.goBackLogin}/>}
                     {this.state.registerHide && <RegisterHide history={this.props.history} returnFX={this.goBackRegister}/>}

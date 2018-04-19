8d82ed8c16814b2825aeb8308e3933f16d6a0bcc
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-19 12:01:03 -0700

    Added header component to grocery list and settings

--- client/src/components/settings/settings.js
+++ client/src/components/settings/settings.js
@@ -4,6 +4,7 @@ import '../../assets/css/settings.css';
 import LogoHeader from '../general/logo-header';
 import Footer from '../general/footer';
 import auth from '../general/auth';
+import Header from '../general/header';
 
 class Settings extends Component {
     constructor(props) {
@@ -23,7 +24,7 @@ class Settings extends Component {
             <div className='settingsContainer'>
                 <LogoHeader />
                 <div className="mainContainer container">
-                    <h4 className='card-panel settingsHeader ubuntuFont z-depth-2 grey darken-2 white-text'>Settings</h4>
+                    <Header title={'Settings'} />
                     <Link to='/diet-settings'className='settingsButton oxygenFont btn-large btn  grey darken-1'>Reset Filters</Link>
                     <Link to='meal-num-settings' className='settingsButton oxygenFont btn-large btn  grey darken-1'>Add Meals</Link>
                     <div className='settingsButton oxygenFont btn btn-large grey darken-1' onClick={this.logoutClicked}>Logout</div>                    

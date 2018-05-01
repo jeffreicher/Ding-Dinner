39425a4ae28eafe1de4361c9480efce927463529
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-30 18:10:13 -0700

    Reformatted settings tab so that it fits on desktop

--- client/src/components/meals/meals.js
+++ client/src/components/meals/meals.js
@@ -76,9 +76,9 @@ class Meals extends Component {
 
             axios({
 
-                // url: 'http://localhost:8080/frontend/Ding-FINAL/endpoints/meals/userCurrentMeals.php',
+                url: 'http://localhost:8080/frontend/Ding-FINAL/endpoints/meals/userCurrentMeals.php',
                 // url: 'http://localhost:8080/C1.18_FoodTinder/endpoints/meals/userCurrentMeals.php',
-                url: '../../endpoints/meals/userCurrentMeals.php',
+                // url: '../../endpoints/meals/userCurrentMeals.php',
                 method: 'post',
                 data: {
                     session_ID: localStorage.ding_sessionID

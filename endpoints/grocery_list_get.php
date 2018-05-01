e9a5bad60167a12e3f2226cf31c5a4f7bdc97af7
KaneMJ <matthewjkane@live.com>
2018-05-01 14:42:38 -0700

    Added endpoint to retrieve the user's grocery list

--- endpoints/grocery_list_get.php
+++ endpoints/grocery_list_get.php
@@ -17,11 +17,34 @@ header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 
 //Test variables for diet update
-// $_SESSION['user_id'] = 31;
-// $request_data['diet'] = "ketogenic";
-// $request_data['allergies'] = ['peanut', 'tree nut', 'ninjas', 'shellfish'];
+$_SESSION['user_id'] = 23;
 
 //Check if user ID is a valid integer. This might be a $_SESSION['user_id'] later
 if(!is_int($_SESSION['user_id'])){
     die('User ID is invalid');
+} 
+$user_id = $_SESSION['user_id'];
+
+//Prepared statement to SELECT user with matching email and password
+if(!($stmt = $myconn->prepare("SELECT `ingredient`, `complete` FROM `grocery_list` WHERE `user_id` = ?"))){
+    die("Prepare failed: (" . $myconn->errno . ") " . $myconn->error);
 }
+
+//Binding parameters for the SELECT statement
+if(!$stmt->bind_param('i', $user_id)){
+    die("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+}
+
+//Execute the prepared SELECT query
+if(!$stmt->execute()){
+    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+}
+
+$result = $stmt->get_result();
+if($result->num_rows === 0) die('No ingredients');
+
+$row = $result->fetch_all(MYSQLI_ASSOC);
+
+//Send results to front end
+$user_grocery_list = json_encode($row);
+print_r($user_grocery_list);

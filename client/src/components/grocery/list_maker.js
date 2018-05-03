54cc0c789a0c3b63f09b279982209832b27af246
Nolan <nemerson17@gmail.com>
2018-05-02 18:36:23 -0700

    Updated grocery list component to save checkmarks

--- client/src/components/grocery/list_maker.js
+++ client/src/components/grocery/list_maker.js
@@ -24,10 +24,6 @@ class ListMaker extends Component{
                 <input type='checkbox' checked={complete ? 'checked' : ''} onChange={this.toggleComplete.bind(this)} className='check filled-in' style={{borderColor: 'white'}} id={this.props.id} />
                 <label className='filled-in name oxygenFont' style={{fontSize: `1.2rem`}} htmlFor={this.props.id}>{this.props.item.ingredient}</label>
             </div>
-            // <p>
-            //     <input type="checkbox" class="filled-in" id="filled-in-box" checked="checked" />
-            //     <label for="filled-in-box">Filled in</label>
-            // </p>
         )
     }
 }

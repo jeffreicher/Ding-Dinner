a5a2ad997bdfa8062ed0e00dd7f3d0428916fac5
Nolan <nemerson17@gmail.com>
2018-05-08 10:19:41 -0700

    Updated grocery list for multiple selections and to grey during toggle

--- client/src/components/grocery/list_maker.js
+++ client/src/components/grocery/list_maker.js
@@ -4,7 +4,13 @@ class ListMaker extends Component{
     constructor(props){
         super(props);
 
+        this.toggleComplete = this.toggleComplete.bind(this);
+        this.resetStyling = this.resetStyling.bind(this);
 
+        this.state = {
+            completing: '',
+            style: 'auto'
+        }
     }
 
     toggleComplete(){
@@ -12,7 +18,20 @@ class ListMaker extends Component{
 
         complete = 1 - complete;
 
-        this.props.completeItem(id, complete);
+        this.setState({
+            completing: 'checkbox-grey',
+            style: 'none'
+        });
+
+        this.props.completeItem(id, complete, this.resetStyling);
+
+    }
+
+    resetStyling() {
+        this.setState({
+            completing: '',
+            style: 'auto'
+        });
     }
 
     render(){
@@ -21,7 +40,7 @@ class ListMaker extends Component{
 
         return (
             <div className='item' key={this.props.id} >
-                <input type='checkbox' checked={complete ? 'checked' : ''} onChange={this.toggleComplete.bind(this)} className='check filled-in' style={{borderColor: 'white'}} id={this.props.id} />
+                <input type='checkbox' checked={complete ? 'checked' : ''} onChange={this.toggleComplete.bind(this)} className={`check filled-in ${this.state.completing}`} style={{pointerEvents: this.state.style}} id={this.props.id} />
                 <label className='filled-in name oxygenFont' style={{fontSize: `1.2rem`}} htmlFor={this.props.id}>{this.props.item.ingredient}</label>
             </div>
         )
